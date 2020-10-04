29753e2c89d1e48febe7b32f0805b922003c59e6
sakiy <skyksun3@gmail.com>
2020-10-04 21:22:59 +0900

    ウィザード形式によるユーザーとプロフィールの登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :birthdat, presence: true
+  validates :birthday, presence: true
 
   has_one :profile
 end
