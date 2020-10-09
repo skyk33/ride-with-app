22ca22d21c9f5db4079cc4654cf0b0dfa68db55b
sakiy <skyksun3@gmail.com>
2020-10-09 15:28:16 +0900

    マイページへのアクセス制限追加、ビューの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_user
+
   def show
-    @user = current_user
-    @profile = current_user.profile
+    # binding.pry
+    @profile = @user.profile
+  end
+
+  private
+
+  def check_user
+    @user = User.find(params[:id])
+    # binding.pry
+    redirect_to root_path, alert: '無効なアクセスです' unless current_user == @user
   end
 end
