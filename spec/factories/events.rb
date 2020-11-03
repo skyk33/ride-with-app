c2e9abf943c04fe8ee8f1e43acf3f0883009b5f2
sakiy <skyksun3@gmail.com>
2020-11-03 16:58:14 +0900

    eventモデル単体テストコード

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
   factory :event do
+    title { Faker::Movies::HarryPotter.book }
+    date { '2000-01-11' }
+    prefecture_id { 1 }
+    meeting_point { Faker::Movies::HarryPotter.location }
+    details { Faker::Lorem.paragraphs }
+    meetup_time { '08:00:00' }
+    association :user
   end
 end
